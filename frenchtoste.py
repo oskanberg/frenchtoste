cc5a2459c6b519bbd32ca82e3cb900a965ef15e9
ost500 <ost500@cs.york.ac.uk>
2013-01-23 00:16:34 +0000

    implement multithreading

--- frenchtoste.py
+++ frenchtoste.py
@@ -1,155 +1,267 @@
 #!/usr/bin/env python
 
 #TODO: - when there are many suggestions, display all? select highest?
-#      - convert &gt; to "> "
 #      - multithreading.py - searcher and queue of possibilies
 
+from multiprocessing import Process, Lock
 import praw
 import time
+import sys
+import os
+import random
 
-USER = "french_toste"
-PASS = "ftftftftftft"
+DATA = os.path.abspath(os.path.join(os.path.curdir, 'suggestions'))
+#USER = 'french_toste'
+USER = 'albert_hindsight'
+#PASS = 'ftftftftftft'
+PASS = 'albert_hindsight'
+DESCRIPTION = 'albert_hindsight v0.1 by /u/blackmirth: scans for popular information posted in reposts to consolidate information.'
 
-class CommentSuggestion:
+
+class CommentSuggestion(object):
     
     def __init__(self, originalCommentObject, submission):
-        self.commentBody        = originalCommentObject.body
-        self.commentAuthor      = originalCommentObject.author
-        self.originalSubmission = originalCommentObject.submission
-        self.originalScore      = originalCommentObject.score
-        self.newSubmission      = submission
+        # Especially stringent so it can get pickled
+        self.commentID    = str(originalCommentObject.permalink)
+        self.submissionID = str(submission.id)
+        self.prospect     = int(self.getSubmissionObject().score)
+    
+    def getCommentObject(self):
+        repickler = praw.Reddit('repickler')
+        for attempt in xrange(5):
+            try:
+                obj = repickler.get_submission(submission_id = self.commentID).comments[0]
+                return obj
+            except Exception, e:
+                time.sleep(random.randint(10))
 
-class FrenchToste(object):
+    def getSubmissionObject(self):
+        repickler = praw.Reddit('repickler')
+        for attempt in xrange(5):
+            try:
+                obj = repickler.get_submission(submission_id = self.submissionID)
+                return obj
+            except Exception, e:
+                time.sleep(random.randint(10))
+
+
+class FrenchTosteBrain(object):
     
-    def __init__(self):
-        self.lastPostTime   = time.time()
-        self.name           = "FrenchToste"
-        self.username       = USER
-        self.password       = PASS
-        self.r              = praw.Reddit(user_agent=self.name) 
+    def __init__(self, r, lock):
+        self.lock           = lock
+        self.debug          = True
+        self.db             = DATA
+        self.r              = r
     
-    def login(self):
-        print "Logging in ... "
-        self.r.login(self.username, self.password)
+    def store_suggestion(self, suggestion):
+        # stores in new line as <submission>:<comment>:<prospect>
+        with self.lock:
+            with open(self.db, "a") as f:
+                f.write("%s;%s;%s\n" % (suggestion.submissionID, suggestion.commentID, suggestion.prospect))
+        
+    def debugPrint(self, msg):
+        if self.debug:
+            print msg
         
     def get_comment_suggestions_for_post(self, post):
-        print "Getting suggestions for:\n", str(post)
+        self.debugPrint('Getting suggestions for:\n%s' % str(post))
         duplicates = self.search_for_duplicates(post)
         if len(duplicates) > 0:
             duplicates = self.apply_post_filters(duplicates)
             suggestions = []
             for dup in duplicates:
-                comments = list(dup.comments)
+                try:
+                    comments = list(dup.comments)
+                except Exception, e:
+                    self.debugPrint('Forbidden. Ignoring.')
+                    self.debugPrint(e)
+                    continue
                 if len(comments) > 0:
                     try:
                         comments = sorted(comments, key=lambda x: x.score, reverse=True)
                     except Exception, e:
-                        print "Comments not loading correctly. Ignoring post."
+                        self.debugPrint('Comments not loading correctly. Ignoring.')
+                        self.debugPrint(e)
                         continue
                     suggestion = CommentSuggestion(comments[0], post)
                     suggestions.append(suggestion)
                     suggestions = self.apply_comment_filters(suggestions)
             if not suggestions:
-                print "No suggestions."
-            suggestions = sorted(suggestions, key=lambda x: x.originalScore, reverse=True)
+                self.debugPrint('No suggestions.')
+            suggestions = sorted(suggestions, key=lambda x: x.getCommentObject().score, reverse=True)
             return suggestions
         else:
-            print "No duplicates."
+            self.debugPrint('No duplicates.')
             return []
     
     def search_for_duplicates(self, post):
-        print "Checking for duplicates:\n%s" % post.url
+        self.debugPrint('Checking for duplicates:\n%s' % post.url)
         try:
             # will fail if result is len 1
             duplicates = list(self.r.search(post.url))
         except Exception, e:
             return []
         if post in duplicates:
-            print "Removing original post ..."
+            self.debugPrint('Removing original post ...')
             duplicates.remove(post)
         return duplicates
     
     def apply_post_filters(self, posts):
-        print "Applying post filters ..."
+        self.debugPrint('Applying post filters ...')
         for post in posts:
-            if "x-post" in post.title or "xpost" in post.title:
-                print "Removing xpost."
+            if 'x-post' in post.title or 'xpost' in post.title:
+                self.debugPrint('Removing xpost.')
                 posts.remove(post)
         return posts
     
     def apply_comment_filters(self, comments):
-        print "Applying comment filters ..."
+        self.debugPrint('Applying comment filters ...')
         for comment in comments:
-            if comment.originalSubmission.author == comment.commentAuthor:
-                print "Removing self comment."
+            co = comment.getCommentObject()
+            if co.submission.author == co.author:
+                self.debugPrint('Removing self comment.')
                 comments.remove(comment)
         return comments
                 
+    def space(self):
+        self.debugPrint('\n')
+    
+    def intelligent_search(self, threshold):
+        self.debugPrint('Warning: this might take a long time and will continue indefinitely.')
+        while True:
+            self.debugPrint('Searching ...')
+            try:
+                post = self.r.get_subreddit('random').get_hot(limit=1)
+            except Exception, h:
+                self.debugPrint("HTTP error:\n" % h)
+                self.debugPrint("Ignoring.")
+                continue
+            try:
+                post = post.next()
+            except Exception, e:
+                self.debugPrint("Error getting next post.")
+            suggestions = self.get_comment_suggestions_for_post(post)
+            for suggestion in suggestions:
+                if suggestion.getCommentObject().score < threshold:
+                    continue
+                else:
+                    self.store_suggestion(suggestion)
+            self.space()
+    
+    def set_output_file(self, outputFile):
+        self.db = outputFile
+
+
+class FrenchToste(object):
+    
+    def __init__(self, brains, lock):
+        self.lastPostTime   = 0
+        self.username  = USER
+        self.password  = PASS
+        self.r         = praw.Reddit(DESCRIPTION)
+        self.brains    = [FrenchTosteBrain(self.r, lock) for i in xrange(brains)]
+        print 'Logging in ...'
+        self.r.login(self.username, self.password)
+    
+    def find_suggestions(self, threshold, outputFile):
+        print "Outputting to: %s" % os.path.join(os.getcwd(), outputFile)
+        try:
+            with open(outputFile, "a+") as f: pass
+        except IOError as e:
+            print "IO Error:", e
+        for brain in self.brains:
+            brain.set_output_file(outputFile)
+            Process(target = brain.intelligent_search, args = (threshold,)).start()
+    
+    def post_comment(self, submissionID, commentID):
+        while time.time() - self.lastPostTime <= 600:
+            print '%ss ...' % (600 - (time.time() - self.lastPostTime))
+            time.sleep(1)
+        print 'Regenerating comment ...'
+        submission = self.r.get_submission(submission_id = submissionID)
+        commmentBody = self.r.get_submission(submission_id = commentID).comments[0].body.replace('&gt;', '> ')
+        time.sleep(5)
+        print 'Posting comment ...'
+        try:
+            comment.getSubmissionObject().add_comment(newComment)
+        except Exception, e:
+            print 'Posting failed. You might be posting too often. Retrying.'
+            print e
+            self.lastPostTime = time.time()
+            self.post_comment(submissionID, commentID)
+        self.lastPostTime = time.time()
+        print 'Done.'
+
+
+class SuggestionReader(object):
+    
+    def __init__(self, inputFile, ft, lock):
+        self.inputFile = inputFile
+        self.ft        = ft
+        self.lock      = lock
+    
+    def space(self):
+        print '\n'
+
     def prompt(self):
         self.space()
-        print "Submit comment? [y/N]"
+        print 'Submit comment? [y/N]'
         while True:
             ans = raw_input()
             if not ans:
                 return resp
             if ans not in ['y', 'Y', 'n', 'N']:
+                print 'got \"%s\"' % ans
                 print 'please enter y or n.'
                 continue
             if ans == 'y' or ans == 'Y':
                 return True
             if ans == 'n' or ans == 'N':
                 return False
-                
-    def suggest(self, suggestion):
+
+    def suggest(self, so, co):
         self.space()
-        print "############## Suggestion ##############"
-        print "commentBody: %s\nnewSubmission: %s\noriginalSubmission: %s\noriginalScore: %s" \
-            % (suggestion.commentBody, suggestion.newSubmission, suggestion.originalSubmission, suggestion.originalScore)
+        print '############## Suggestion ##############'
+        print 'commentBody: %s'         % co.body
+        print 'newSubmission: %s'       % so
+        print 'originalSubmission: %s'  % co.submission
+        print 'originalScore: %s'       % co.score
         if self.prompt():
-            self.post_comment(suggestion)
+            self.ft.post_comment(suggestion)
         self.space()
-                    
-    def space(self):
-        print "\n"
     
-    def post_comment(self, comment):
-        while self.lastPostTime - time.time() >= 600:
-            print "%ss ..." % ((time.lastPostTime - time.time()) - 600)
-            time.sleep(1)
-        print "Posting comment ..."
-        comment.commentBody = comment.commentBody.replace("&gt;", "> ")
-        self.login()
-        try:
-            comment.newSubmission.add_comment(comment.commentBody)
-        except Exception, e:
-            print "Posting failed. You might be posting too often. Retrying."
-            self.lastPostTime = time.time()
-            self.post_comment(comment)
-        self.lastPostTime = time.time()
-        print "Done."
-    
-    def intelligent_search(self, threshold):
-        print "Warning: this might take a long time and will continue indefinitely."
-        while True:
-            print "Searching ..."
-            post        = self.r.get_subreddit("random").get_hot(limit=1)
-            suggestions = self.get_comment_suggestions_for_post(post.next())
-            for suggestion in suggestions:
-                if suggestion.originalScore < threshold:
-                    continue
-                else:
-                    self.suggest(suggestion)
-            self.space()
+    def load_suggestion_strings(self):
+        print self.inputFile
+        with self.lock:
+            with open(self.inputFile, "r") as f:
+                sugs = []
+                for sug in f:
+                    sugs.append(sug)
+        return sugs
         
-    def check_subreddits(self, subreddits):
-        for sr in subreddits:
-            post        = self.r.get_subreddit(sr).get_hot(limit=1)
-            suggestions = self.get_comment_suggestions_for_post(post.next())
-            for suggestion in suggestions:
-                self.suggest(suggestion)
-            self.space()
+    def loop(self):
+        while True:
+            time.sleep(10)
+            os.system(['clear', 'cls'][os.name == 'nt'])
+            if os.stat(self.inputFile).st_size > 0:
+                suggestions = self.load_suggestion_strings()
+                for suggestion in suggestions:
+                    args = suggestion.split(";")
+                    self.ft.post_comment(args[0], args[1])
+            else:
+                print "No items available yet."
 
-ft = FrenchToste()
-#ft.check_subreddits(["pics", "funny", "all", "random"])
-ft.intelligent_search(15)
+    
+def main():
+    lock = Lock()
+    ft = FrenchToste(0, lock)
+    sr = SuggestionReader(DATA, ft, lock)
+    ft.find_suggestions(50, DATA)
+    sr.loop()
+    #r = praw.Reddit(DESCRIPTION)
+    #r.login(USER,PASS)
+    #a = r.get_subreddit("random").get_hot(1)
+    #a.next().add_comment("hello")
 
+if __name__ == '__main__':
+    main()
